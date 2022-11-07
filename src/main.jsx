2d80ddedbdba140df5c8936437c81345fe8e2fbc
bruno-lupi <lupi.bruno@outlook.com>
2022-11-07 19:48:57 -0300

    feat: importado componente CSS Baselines do MUI

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/index.css'
+import { CssBaseline } from '@mui/material';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <React.StrictMode>z
+    <CssBaseline />
     <App />
   </React.StrictMode>
 )
