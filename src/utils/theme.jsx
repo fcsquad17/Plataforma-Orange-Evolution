cf58327c2d26febb11b893ba70f741ee03224730
bruno-lupi <lupi.bruno@outlook.com>
2022-11-08 12:51:01 -0300

    style: atualiza paleta de cores

--- src/utils/theme.jsx
+++ src/utils/theme.jsx
@@ -3,7 +3,8 @@ import { createTheme } from "@mui/material";
 export const theme = createTheme({
     palette: {
       primary: {
-        main: '#001024'
+        main: '#001024',
+        soft: '#202C3B'
       },
       secondary: { 
         main: '#00C19C'
