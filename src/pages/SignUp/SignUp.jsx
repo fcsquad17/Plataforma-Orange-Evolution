0143aae96b8fc7e097ab411d8709db2c1449d6a7
WalterDSTS <waltersantossimoni@gmail.com>
2022-11-08 17:36:58 -0300

    feat: Criado a página de SignUp.

---
+++ src/pages/SignUp/SignUp.jsx
@@ -0,0 +1,13 @@
+import Header from '/src/components/Header/Header'
+import SignUpScreen from '/src/components/SignUpScreen/SignUpScreen'
+import Footer from '/src/components/Footer/Footer'
+
+export default function SignUp() {
+  return (
+    <div>
+      <Header />
+      <SignUpScreen />
+      <Footer />
+    </div>
+  )
+}
