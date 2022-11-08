59a59e1bd71c33d350e2385b5223b6c2ba68940b
WalterDSTS <waltersantossimoni@gmail.com>
2022-11-08 16:06:40 -0300

    feat: Criado a página de Login.

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '/src/components/Header/Header'
+import LoginScreen from '/src/components/LoginScreen/LoginScreen'
+import Footer from '/src/components/Footer/Footer'
+
+export default function Login() {
+  return (
+    <div>
+      <Header />
+      <LoginScreen />
+      <Footer />
+    </div>
+  )
+}
