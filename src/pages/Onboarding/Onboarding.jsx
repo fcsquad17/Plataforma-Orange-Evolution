8ff133cea208b86837fef53d40d3e3f8f00ed2af
bruno-lupi <lupi.bruno@outlook.com>
2022-11-09 11:20:08 -0300

    feat: atualizada composição da page Onboarding

--- src/pages/Onboarding/Onboarding.jsx
+++ src/pages/Onboarding/Onboarding.jsx
@@ -1,35 +1,15 @@
 import Header from '/src/components/Header/Header'
-import TrailsCard from '/src/components/TrailsCard/TrailsCard'
 import Footer from '/src/components/Footer/Footer'
-
-// Import style (s = style)
-import s from "./Onboarding.module.css";
+import { IntroOnboarding } from '../../components/IntroOnboarding/IntroOnboarding'
 
 function Onboarding() {
     return (
-      <>
-        <div>
-          <Header /> 
-        </div>
-        <div className={s.intro}>
-          <div className={s.divLogo}>
-              <img src='/src/assets/oeLogo.svg' alt='Orange Evolution' className={s.oeLogo}></img>
-          </div>
-          <div className={s.divEvolua}>
-            <h1 className={s.h1}>Evolua a sua carreira na tecnologia</h1>
-            <span className={s.span}>Explore conhecimentos que estão transformando 
-                indústrias, negócios e vidas através de</span><strong className={s.strong}> trilhas
-                gratuitas em Desenvolvimento, UX/UI Design e QA!
-            </strong>
-          </div>
-        </div>
-       <div className={s.cards}>
-          <TrailsCard />
-        </div> 
-        <div>
+      <div>       
+          <Header />
+          <IntroOnboarding />        
           <Footer />
-        </div>  
-      </>
+      </div>  
+      
     )
   }
   
