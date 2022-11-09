ea2f1c038a7bb008a52e4732fcab55cded00bc6b
WalterDSTS <waltersantossimoni@gmail.com>
2022-11-09 12:46:07 -0300

    feat: Criado a página Trails.

---
+++ src/pages/Trails/Trails.jsx
@@ -0,0 +1,13 @@
+import Header from "../../components/Header/Header"
+import TrailsScreen from "../../components/TrailsScreen/TrailsScreen"
+import Footer from "../../components/Footer/Footer"
+
+export default function Trails() {
+  return (
+    <div>
+      <Header />
+      <TrailsScreen />
+      <Footer />
+    </div>
+  )
+}
