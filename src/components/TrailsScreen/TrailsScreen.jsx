a44f0c36ee26002f599af44ac4db6a0f9bf28e80
hudson-uchoa <uchoa.hudson1@gmail.com>
2022-11-09 22:18:40 -0300

    feat: logica com requisição iniciado na trailscreen

--- src/components/TrailsScreen/TrailsScreen.jsx
+++ src/components/TrailsScreen/TrailsScreen.jsx
@@ -1,22 +1,53 @@
 import s from "/src/components/TrailsScreen/TrailsScreen.module.css";
+import TrailsCard from "/src/components/TrailsCard/TrailsCard";
+import TrailsCardUser from "../TrailsCardUser/TrailsCardUser";
+import { useEffect, useState } from "react";
+import {
+  getContentOfUserByTrailId,
+  getUserTrailsParams,
+} from "../../services/Api";
 
-import TrailsCard from '/src/components/TrailsCard/TrailsCard'
+export default function TrailsScreen({ user }) {
+  const [userTrails, setUserTrails] = useState([]);
 
-import { useState } from "react";
+  const handleOnReq = async (idUser) => {
+    const response = await getUserTrailsParams(idUser);
+    setUserTrails(response.trilhas);
+  };
 
-export default function TrailsScreen() {
-  const [userData, setUserData] = useState({
-    name: "Walter dos Santos",
-    email: "walter@123.com",
-  });
+  const handleOnReq2 = async (idUser, idTrail) => {
+    const response = await getContentOfUserByTrailId(idUser, idTrail);
+    const filterContentId = response.conteudos.filter((conteudo) => {
+      return conteudo.ID;
+    });
+    console.log(await filterContentId);
+  };
+
+  useEffect(() => {
+    handleOnReq(user.ID);
+  }, []);
+
+  useEffect(() => {
+    const filterTrailId = userTrails.map((object) => object.ID);
+    filterTrailId.map((trail) => handleOnReq2(user.ID, trail));
+  }, [userTrails]);
 
   return (
     <div className={s.container}>
       <div className={s.wrapper}>
-        <h2 className={s.h2}>Que bom te ver de novo, {userData.name}!</h2>
+        <h2 className={s.h2}>
+          Que bom te ver de novo,{" "}
+          {user.NOME_COMPLETO ? user.NOME_COMPLETO.split(" ")[0] : null}!
+        </h2>
         <h4 className={s.h4}>Continue de onde parou:</h4>
-        <TrailsCard />
-        <hr className={s.hr}/>
+        <div className={s.cardStyle}>
+          {userTrails.map((card) => {
+            return (
+              <TrailsCardUser key={card.ID} trail={card} progressNumber={30} />
+            );
+          })}
+        </div>
+        <hr className={s.hr} />
         <h2 className={s.h2}>Conheça nossas demais trilhas:</h2>
         <TrailsCard />
       </div>
