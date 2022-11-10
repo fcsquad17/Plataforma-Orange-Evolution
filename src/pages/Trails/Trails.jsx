3dc352748ec7907f15400de28a82f889edaf45ad
hudson-uchoa <uchoa.hudson1@gmail.com>
2022-11-09 22:20:03 -0300

    feat: logica com requisição iniciado na pagina de trilhas

--- src/pages/Trails/Trails.jsx
+++ src/pages/Trails/Trails.jsx
@@ -1,16 +1,34 @@
 import Header from "../../components/Header/Header";
 import TrailsScreen from "../../components/TrailsScreen/TrailsScreen";
 import Footer from "../../components/Footer/Footer";
+import { useEffect, useState } from "react";
+import { getUsersParams } from "../../services/Api";
 
 export default function Trails() {
+  const [user, setUser] = useState({});
+  const [hasLoaded, setHasLoaded] = useState(false);
+
+  const handleReq = async () => {
+    const response = await getUsersParams(1);
+    setUser(response.usuario);
+  };
+
+  useEffect(() => {
+    handleReq();
+  }, []);
+
+  useEffect(() => {
+    if (user.ID) setHasLoaded(true);
+  }, [user]);
   return (
     <div>
       <Header
         pages={["Inicio", "Trilhas"]}
         settings={["Meu dados", "Sair"]}
-        userName={""}
+        userName={user.NOME_COMPLETO}
       />
-      <TrailsScreen />
+      {hasLoaded && <TrailsScreen user={user} />}
+
       <Footer />
     </div>
   );
