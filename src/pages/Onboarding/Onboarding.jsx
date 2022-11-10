b1943d018bbd2fd9db7a9334ae0319c9299c0a8b
hudson-uchoa <uchoa.hudson1@gmail.com>
2022-11-10 16:03:11 -0300

    feat: logica para onboarding não deslogar o usuario caso clique no botão de 'inicio' do header

--- src/pages/Onboarding/Onboarding.jsx
+++ src/pages/Onboarding/Onboarding.jsx
@@ -1,16 +1,51 @@
 import Header from "/src/components/Header/Header";
 import Footer from "/src/components/Footer/Footer";
 import { OnboardingScreen } from "../../components/OnboardingScreen/OnboardingScreen";
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { getUsersParams } from "../../services/Api";
 
 function Onboarding() {
+  const [user, setUser] = useState({});
+
+  const { id } = useParams();
+  const idUser = localStorage.getItem("idUser");
+
+  const handleReq = async () => {
+    if (idUser) {
+      const response = await getUsersParams(idUser);
+      setUser(response.usuario);
+    }
+  };
+
+  useEffect(() => {
+    handleReq();
+  }, []);
+
   return (
     <div>
-      <Header
-        pages={["Inicio"]}
-        settings={["Entrar", "Cadastrar"]}
-        userName={""}
-        url={['/', '/', '/login', '/signup']}
-      />
+      {id && idUser && (
+        <Header
+          pages={["Inicio", "Trilhas"]}
+          settings={["Meu dados", "Sair"]}
+          userName={user.NOME_COMPLETO}
+          url={[
+            `/${localStorage.getItem("idUser")}`,
+            `/trails/${localStorage.getItem("idUser")}`,
+            `/profile/${localStorage.getItem("idUser")}`,
+            "/",
+          ]}
+        />
+      )}
+      {!id && (
+        <Header
+          pages={["Inicio"]}
+          settings={["Entrar", "Cadastrar"]}
+          userName={""}
+          url={["/", "/", "/login", "/signup"]}
+        />
+      )}
+
       <OnboardingScreen />
       <Footer />
     </div>
