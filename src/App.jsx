b89a6ba65a605c295040929ab4ab4e86834a00e1
WalterDSTS <waltersantossimoni@gmail.com>
2022-11-11 18:56:22 -0300

    feat: Criado o route para á página dos Administradores.

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import TrailsContent from "./pages/TrailsContent/TrailsContent";
 import Login from "./pages/Login/Login";
 import SignUp from "./pages/SignUp/SignUp";
 import Profile from "./pages/Profile/Profile";
+import AdminControl from "./pages/AdminControl/AdminControl";
 import NotFound from "./pages/NotFound/NotFound";
 import ForgotPassword from "./pages/ForgotPassword/ForgotPassword";
 
@@ -25,6 +26,7 @@ function App() {
           <Route path="/signup" element={<SignUp />} />
           <Route path="/forgotpassword" element={<ForgotPassword />} />
           <Route path="/profile/:id" element={<Profile />} />
+          <Route path="/admin" element={<AdminControl />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
