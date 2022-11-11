900361a6a5360c4aced02c8beefa3b341b1637a0
hudson-uchoa <uchoa.hudson1@gmail.com>
2022-11-10 22:25:20 -0300

    feat: logica da pagina de conteudo das trilhas semi-concluido

--- src/components/AllModules/AllModules.jsx
+++ src/components/AllModules/AllModules.jsx
@@ -1,17 +1,41 @@
-import TrailsModules from '../TrailsModules/TrailsModules'
-import s from '/src/components/AllModules/AllModules.module.css'
-
-
+import TrailsModules from "../TrailsModules/TrailsModules";
+import s from "/src/components/AllModules/AllModules.module.css";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { getModuleByIdTrail, getTrailParams } from "../../services/Api";
 
 export default function AllModules() {
-  const data = {
-    name: 'Desenvolvimento FullStack'
-  }
+  const [trail, setTrail] = useState({});
+  const [modules, setModules] = useState([]);
+
+  const { idTrail } = useParams();
+
+  const handleReq = async (idTrail) => {
+    const response = await getTrailParams(idTrail);
+    setTrail(response.trilha);
+    const response2 = await getModuleByIdTrail(idTrail);
+    setModules(response2.modulos);
+  };
+
+  useEffect(() => {
+    handleReq(idTrail);
+  }, []);
+
   return (
     <div className={s.container}>
-      <h2 className={s.h2}>Seja bem vindo (a) à trilha sobre {data.name}!</h2>
-      <h4 className={s.h4}>Conteúdos:</h4>
-      <TrailsModules />
+      <h2 className={s.h2}>
+        Seja bem vindo (a) à trilha sobre {trail.TITULO}!
+      </h2>
+      {modules.map((module) => {
+        return (
+          <TrailsModules
+            TITULO={module.TITULO}
+            DESCRICAO={module.DESCRICAO}
+            idModulo={module.ID}
+            key={module.ID}
+          />
+        );
+      })}
     </div>
-  )
+  );
 }
