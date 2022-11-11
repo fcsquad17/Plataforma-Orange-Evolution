22386e851d142ba5ea3c3e05cca96cf08913b46f
bruno-lupi <lupi.bruno@outlook.com>
2022-11-11 01:29:22 -0300

    feat: cria página de reset de senha

---
+++ src/pages/ResetPassword/ResetPassword.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '/src/components/Header/Header.jsx'
+import ResetPasswordScreen from '/src/components/ResetPasswordScreen/ResetPasswordScreen.jsx'
+import Footer from '/src/components/Footer/Footer.jsx'
+
+export const ResetPassword = () => {
+  return (
+    <div>
+        <Header />
+        <ResetPasswordScreen />
+        <Footer />
+    </div>
+  )
+}
