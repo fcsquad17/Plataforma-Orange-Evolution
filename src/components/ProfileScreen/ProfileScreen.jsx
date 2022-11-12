1f4017614683ef6667d3bd9b0533da3a9d1cd1d1
hudson-uchoa <uchoa.hudson1@gmail.com>
2022-11-11 21:59:44 -0300

    feat: logica da mudança de informações de login do usuario com requisição feita

--- src/components/ProfileScreen/ProfileScreen.jsx
+++ src/components/ProfileScreen/ProfileScreen.jsx
@@ -1,12 +1,46 @@
 import { useState } from "react";
 import s from "/src/components/ProfileScreen/ProfileScreen.module.css";
 import { motion } from "framer-motion";
+import { TextField } from "@mui/material";
+import { putUser } from "../../services/Api";
 
-export default function ProfileScreen({ user }) {
+export default function ProfileScreen({ user, handleReload }) {
   const [clicked, setClicked] = useState(false);
+  const [error, setError] = useState(false);
+  const [password, setPassword] = useState("");
+  const [userBody, setUserBody] = useState({
+    nome: user.NOME_COMPLETO,
+    email: user.EMAIL,
+    senha: "",
+    admin: 0,
+  });
+
+  const handleChange = (target, key) => {
+    const value = target.value;
+    setUserBody({ ...userBody, [key]: value });
+  };
+
+  const handleChangePass = (target) => {
+    const value = target.value;
+    setPassword(value);
+  };
 
   const handleClick = () => {
     setClicked(true);
+  };
+
+  const handleOnSubmit = async (e) => {
+    e.preventDefault();
+    if (userBody.senha !== password) {
+      setError(true);
+      return false;
+    }
+
+    await putUser(user.ID, userBody);
+
+    setClicked(false);
+
+    handleReload();
   };
 
   return (
@@ -38,17 +72,53 @@ export default function ProfileScreen({ user }) {
         style={{ display: clicked ? "flex" : "none" }}
       >
         <h2 className={s.h2}>Altere seus dados</h2>
-        <form className={s.changeDataForm}>
-          <p className={s.userDataItems}>Nome:</p>
-          <input type="text" className={s.inputData} required />
-          <p className={s.userDataItems}>E-mail:</p>
-          <input type="email" className={s.inputData} required />
-          <p className={s.userDataItems}>Nova senha:</p>
-          <input type="password" className={s.inputData} required />
-          <p className={s.userDataItems}>Confirmar senha:</p>
-          <input type="password" className={s.inputData} required />
+        <form className={s.changeDataForm} onSubmit={handleOnSubmit}>
+          <TextField
+            id="filled-basic"
+            label="Nome Completo"
+            variant="filled"
+            className={s.inputData}
+            required
+            sx={{ backgroundColor: "white" }}
+            type={"text"}
+            defaultValue={user.NOME_COMPLETO}
+            onChange={({ target }) => handleChange(target, "nome")}
+          />
+          <TextField
+            id="filled-basic"
+            label="Email"
+            variant="filled"
+            className={s.inputData}
+            required
+            sx={{ backgroundColor: "white" }}
+            type={"email"}
+            defaultValue={user.EMAIL}
+            onChange={({ target }) => handleChange(target, "email")}
+          />
+          <TextField
+            id="filled-basic"
+            label="Nova Senha"
+            variant="filled"
+            className={s.inputData}
+            required
+            sx={{ backgroundColor: "white" }}
+            type={"password"}
+            onChange={({ target }) => handleChange(target, "senha")}
+            error={error}
+          />
+          <TextField
+            id="filled-basic"
+            label="Confirmar senha"
+            variant="filled"
+            className={s.inputData}
+            required
+            sx={{ backgroundColor: "white" }}
+            type={"password"}
+            onChange={({ target }) => handleChangePass(target)}
+            error={error}
+          />
+          <input type="submit" value="Alterar" className={s.changeDataInput} />
         </form>
-        <input type="submit" value="Alterar" className={s.changeDataInput} />
       </motion.div>
     </motion.div>
   );
