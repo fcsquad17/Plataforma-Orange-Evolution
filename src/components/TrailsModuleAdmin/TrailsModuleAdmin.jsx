1997933704604e427baf684f7fdd0c524434719b
hudson-uchoa <uchoa.hudson1@gmail.com>
2022-11-13 03:41:33 -0300

    feat: modal com card dos módulos feita, com requisição do back para renderização e com opção de adicionar, remover e modificar modulos existentes

--- src/components/TrailsModuleAdmin/TrailsModuleAdmin.jsx
+++ src/components/TrailsModuleAdmin/TrailsModuleAdmin.jsx
@@ -4,19 +4,50 @@ import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import DeleteIcon from "@mui/icons-material/Delete";
-import ChildModalAdmin from '../ChildModalAdmin/ChildModalAdmin.';
+import ChildModalAdmin from "../ChildModalAdmin/ChildModalAdmin.";
 import { Container } from "@mui/material";
 import { useState } from "react";
+import ModalForm from "../ModalForm/ModalForm";
+import AlertDialog from "../AlertDialog/AlertDialog";
 
-export default function TrailsModuleAdmin({ title, desc, setSelectedModule, selectedModule, module }) {
-  const [open, setOpen] = useState(false);
-
+export default function TrailsModuleAdmin({
+  title,
+  desc,
+  setSelectedModule,
+  selectedModule,
+  module,
+  handleReload,
+}) {
+  const [selectedModuleDelete, setSelectedModuleDelete] = useState(0);
+  const [selectedModuleEdit, setSelectedModuleEdit] = useState(0);
   const handleOpen = (module) => {
-    setSelectedModule(selectedModule => selectedModule.ID === module.ID ? null : module)
+    setSelectedModule((selectedModule) =>
+      selectedModule.ID === module.ID ? null : module
+    );
+  };
+
+  const handleOpenDelete = (module) => {
+    setSelectedModuleDelete((selectedModule) =>
+      selectedModule.ID === module.ID ? null : module
+    );
+  };
+
+  const handleOpenEdit = (module) => {
+    setSelectedModuleEdit((selectedModule) =>
+      selectedModule.ID === module.ID ? 0 : module
+    );
   };
 
   const handleClose = () => {
-    setSelectedModule(0)
+    setSelectedModule(0);
+  };
+
+  const handleCloseDelete = () => {
+    setSelectedModuleDelete(0);
+  };
+
+  const handleCloseEdit = () => {
+    setSelectedModuleEdit(0);
   };
 
   return (
@@ -31,19 +62,40 @@ export default function TrailsModuleAdmin({ title, desc, setSelectedModule, sele
       }}
     >
       <ChildModalAdmin
-        setOpen={setOpen}
         open={selectedModule.ID === module.ID}
         handleClose={handleClose}
         title={module.TITULO}
         idModule={module.ID}
       />
-      <Card sx={{ maxWidth: 375, backgroundColor: "#202C3B"}}>
+      <AlertDialog
+        open={selectedModuleDelete.ID === module.ID}
+        handleClose={handleCloseDelete}
+        title={module.TITULO}
+        sx={{ backgroundColor: "#202C3B" }}
+        handleReload={handleReload}
+        id={module.ID}
+        trail={true}
+      />
+      <ModalForm
+        put={true}
+        module={module}
+        handleClose={handleCloseEdit}
+        open={selectedModuleEdit.ID === module.ID}
+        handleReload={handleReload}
+      />
+      <Card sx={{ maxWidth: 375, backgroundColor: "#202C3B" }}>
         <CardContent>
           <Typography gutterBottom variant="h5" component="div" color="white">
             Título: {title}
           </Typography>
           <Typography>Descrição: {desc}</Typography>
-          <Button variant="contained" sx={{ margin: "5px" }}>
+          <Button
+            variant="contained"
+            sx={{ margin: "5px" }}
+            onClick={() => {
+              handleOpenEdit(module);
+            }}
+          >
             Editar
           </Button>
           <Button
@@ -64,6 +116,9 @@ export default function TrailsModuleAdmin({ title, desc, setSelectedModule, sele
                 backgroundColor: "#420d09",
               },
               margin: "5px",
+            }}
+            onClick={() => {
+              handleOpenDelete(module);
             }}
           >
             Deletar
