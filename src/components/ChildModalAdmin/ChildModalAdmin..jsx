dcd8d9648381696d906a74c525822513a4bcbee2
WalterDSTS <waltersantossimoni@gmail.com>
2022-11-14 17:25:12 -0300

    refactor: Reestruturação dos componentes MUI.

--- src/components/ChildModalAdmin/ChildModalAdmin..jsx
+++ src/components/ChildModalAdmin/ChildModalAdmin..jsx
@@ -4,20 +4,7 @@ import Modal from "@mui/material/Modal";
 import Button from "@mui/material/Button";
 import DataTableAdmin from "../DataTableAdmin/DataTableAdmin";
 import ModalForm from "../ModalForm/ModalForm";
-
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 400,
-  bgcolor: "background.paper",
-  border: "2px solid #000",
-  boxShadow: 24,
-  pt: 2,
-  px: 4,
-  pb: 3,
-};
+import { Typography } from "@mui/material";
 
 export default function ChildModalAdmin({
   open,
@@ -45,38 +32,38 @@ export default function ChildModalAdmin({
   }, [reload]);
 
   return (
-    <React.Fragment>
-      <Modal
-        hideBackdrop
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="child-modal-title"
-        aria-describedby="child-modal-description"
+    <Modal
+      hideBackdrop
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="child-modal-title"
+      aria-describedby="child-modal-description"
+    >
+      <Box
+        sx={{
+          width: "100vw",
+          height: "100vh",
+          p: "15px",
+          color: "#fff",
+          backgroundColor: "#202C3B",
+          overflow: "scroll",
+        }}
       >
-        <Box
-          sx={{
-            ...style,
-            width: "100vw",
-            height: "100vh",
-            color: "#fff",
-            backgroundColor: "#202C3B",
-            overflow: "scroll",
-          }}
-        >
-          <h2 id="child-modal-title">Estes são os conteúdos de: {title}</h2>
-          <ModalForm
-            open={openCreate}
-            content={true}
-            handleClose={handleCloseCreate}
-            handleReload={handleReload}
-            put={false}
-            id={idModule}
-          />
-          <Button onClick={handleClose}>Voltar</Button>
-          <Button onClick={handleOpenCreate}>Criar conteudo</Button>
-          <DataTableAdmin idModule={idModule} />
-        </Box>
-      </Modal>
-    </React.Fragment>
+        <Typography id="child-modal-title" p="6px 8px" variant="h5">
+          Estes são os conteúdos de: {title}
+        </Typography>
+        <ModalForm
+          open={openCreate}
+          content={true}
+          handleClose={handleCloseCreate}
+          handleReload={handleReload}
+          put={false}
+          id={idModule}
+        />
+        <Button onClick={handleClose}>Voltar</Button>
+        <Button onClick={handleOpenCreate}>Criar conteudo</Button>
+        <DataTableAdmin idModule={idModule} />
+      </Box>
+    </Modal>
   );
 }
