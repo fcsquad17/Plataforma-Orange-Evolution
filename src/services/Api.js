fa2a2e2c450f17deb01c8e2d1c36012fe4f4a089
hudson-uchoa <uchoa.hudson1@gmail.com>
2022-11-15 21:35:34 -0300

    feat: adicionado função de configuração das chamadas da api

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,5 +1,20 @@
 import axios from "axios";
 
+// export const Api = axios.create({
+//   baseURL: "https://Api-orange-evolution-production.up.railway.app",
+// });
+
 export const Api = axios.create({
-  baseURL: "https://Api-orange-evolution-production.up.railway.app",
+  baseURL: "http://localhost:3000",
 });
+
+export const globalApiVariables = () => {
+  const userToken = localStorage.getItem("userToken");
+
+  const config = {
+    headers: {
+      Authorization: userToken,
+    },
+  };
+  return config;
+};
